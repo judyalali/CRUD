458bf02374282545687dd1d75cca349479c26a04
judyalali <cudielhalaf016@gmail.com>
2025-05-13 21:32:48 +0300

    add a form tag

--- main.js
+++ main.js
@@ -2,21 +2,36 @@
 let productname = document.getElementById('inputname');
 let productcategory = document.getElementById("input-category");
 let productprice = document.getElementById("input-price");
-let Producdiscount= document.getElementById("inputdiscount");
-let productdescription = document.getElementById("inputdescription")
-let productAddBtn=document.getElementById("productAddBtn")
-let  listpfproduct = [];
+let Productdiscount= document.getElementById("inputdiscount");
+let productdescription = document.getElementById("inputdescription");
+let productAddBtn=document.getElementById("productAddBtn");
+let addbutton = document.querySelector(".addbtn");
+let resetbutton = document.querySelector(".resetbtn");
+let  listpofproduct = [];
 let globalid =0;
 
+
+
 if(localStorage.getItem('allproduct') !=null){
- listpfproduct= JSON.parse(localStorage.getItem ('allproduct')); 
+ listpofproduct= JSON.parse(localStorage.getItem ('allproduct')); 
 displyproduct() 
 
 }
 
+function resetBtn(){
+  productname.value = "";
+  productcategory.value = "";
+  productprice.value = "";
+  Productdiscount.value = "";
+  productdescription.value = "";
 
+}
+resetbutton.onclick = resetBtn;
 
-function createproduct() {
+function createproduct(e) {
+
+e.preventDefault()
+
 
 if (productAddBtn.innerHTML.includes("add")){
 
@@ -24,67 +39,83 @@ if (productAddBtn.innerHTML.includes("add")){
     name:productname.value, 
     category:productcategory.value, 
     price:productprice.value, 
-    discount:Producdiscount.value,
+    discount:Productdiscount.value,
     description:productdescription.value,}
-    listpfproduct.push(product);}
+    listpofproduct.push(product);}
 
 else{
   updateproduct()
 }
-localStorage.setItem('allproduct',JSON.stringify(listpfproduct)  )
+
+localStorage.setItem('allproduct',JSON.stringify(listpofproduct)  )
 displyproduct()
  resetinput()
-console.log(listpfproduct);}
+      productAddBtn.innerHTML = "add product";
+}
+addbutton.onclick = createproduct;
+
+
+
+
 
 function displyproduct() {
 let box = '';
-for (let  i = 0;  i <listpfproduct.length;  i++) {
+for (let  i = 0;  i <listpofproduct.length;  i++) {
     box += 
 `<div class="col-lg-4  " >
 <div    class='box text-capitalize  '  >
 <h5> id: ${i+1}</h5>
-<h5> name: ${listpfproduct[i].name}</h5>
-<h5> category:${listpfproduct[i].category}</h5>
-<h5> price:${listpfproduct[i].price}</h5>
-<h5> discount:${listpfproduct[i].discount}</h5>
-<h5>  description:${listpfproduct[i].description}</h5>
+<h5> name: ${listpofproduct[i].name }  </h5>
+<h5> category:${listpofproduct[i].category}</h5>
+<h5> price:${listpofproduct[i].price}  $ </h5>
+<h5> discount:${listpofproduct[i].discount} %  </h5>
+<h5>  description:${listpofproduct[i].description}</h5>
+
+<button  class=" mt-3  btn-edit    text-capitalize "  onclick="retriveProduct(${i})"       >
+ <i class="fa-solid fa-pen  siz-icon  "     "></i>  </button>
 
 
-<button  class="btn2 mt-3      text-capitalize "  onclick="retriveProduct(${i})" > <i class="fa-solid fa-pen" style="color:rgb(179, 169, 178);"></i>  </button>
-<button  class="btn1 mt-3  text-capitalize    "  onclick="deleteproduct(${i})" > <i class="fa-solid fa-trash" style="color:    rgb(179, 169, 178);"></i></button>
+
+<button  class=" mt-3  text-capitalize  deletbtn  " style="border: none; outline: none;"          onclick="deleteproduct(${i})"   id="deletebutton" > 
+<i class="fa-solid fa-trash btn-delete fa-pen   siz-icon "  "></i></button>
 </div>
 </div> `}
-document.getElementById('rowData').innerHTML=box;}
+document.getElementById('rowData').innerHTML=box;
+}
+
+
+
 
 function resetinput() {
     productname.value=""
     productcategory.value=""
     productprice.value=""
-    Producdiscount.value=""
+    Productdiscount.value=""
     productdescription.value=""
 }
- function deleteproduct(deleteid){
-listpfproduct.splice(deleteid,1)  
-localStorage.setItem('allproduct',JSON.stringify(listpfproduct)  ) 
-displyproduct()
-console.log(listpfproduct);
 
- }
+ function deleteproduct(deleteid){
+listpofproduct.splice(deleteid,1)  
+localStorage.setItem('allproduct',JSON.stringify(listpofproduct)  ) 
+displyproduct()
+console.log(listpofproduct);
+
+}
  function retriveProduct(editid){
   globalid=editid;
- productname.value=listpfproduct[editid].name;
- productcategory.value=listpfproduct[editid].category;
- productprice.value = listpfproduct[editid].price;
-Producdiscount.value=listpfproduct[editid].discount;
-productdescription.value=listpfproduct[editid].description;
+ productname.value=listpofproduct[editid].name;
+ productcategory.value=listpofproduct[editid].category;
+ productprice.value=listpofproduct[editid].price;
+Productdiscount.value=listpofproduct[editid].discount;
+productdescription.value=listpofproduct[editid].description;
 productAddBtn.innerHTML="update product";}
 
 function  updateproduct(){
- listpfproduct [globalid].name=productname.value ;  
- listpfproduct [globalid].category=productcategory.value ;
- listpfproduct[globalid].price=productprice.value ;
- listpfproduct [globalid].discount=Producdiscount.value ;
- listpfproduct [globalid].description=productdescription.value ;
+ listpofproduct [globalid].name=productname.value ;  
+ listpofproduct [globalid].category=productcategory.value ;
+ listpofproduct[globalid].price=productprice.value ;
+ listpofproduct [globalid].discount=Productdiscount.value ;
+ listpofproduct [globalid].description=productdescription.value ;
 
  productAddBtn.innerHTML="add product";
  resetinput()
